62a22fd61efa33234e51fd5b8dfb22800106ed7d
Fayçal BORDJAH <fbordjah@linagora.com>
2020-01-30 17:37:18 +0100

    Add eslnit dependency for control code style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,2 +1,26 @@
-export const root = true;
-export const extends = '@react-native-community';
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
