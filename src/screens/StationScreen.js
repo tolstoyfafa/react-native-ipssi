13b8c9eab063dac71a6340521606d042a3d11b8f
Fayçal BORDJAH <fbordjah@linagora.com>
2020-01-30 16:07:09 +0100

    Add a marker to map view

--- src/screens/StationScreen.js
+++ src/screens/StationScreen.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 
 
 
 
-export default function StationScreen(props) {
+export default function StationScreen({ navigation }) {
 
     const styles = StyleSheet.create({
         container: {
             flex: 1,
             backgroundColor: '#fff',
+        },
+        title: {
+            textAlign: 'center',
+            fontStyle: 'italic',
+            fontSize: 30
         },
         map: {
             flex: 1,
@@ -26,41 +31,80 @@ export default function StationScreen(props) {
             fontSize: 20,
             textAlign: 'center',
             color: 'palevioletred'
+        },
+        imageStyle: {
+            height: 6,
+            width: 6
         }
     });
 
-    const data = props.navigation.getParam('stationDetails', 'default');
+    const data = navigation.getParam('stationDetails', 'default');
 
+    const {
+        creditcard,
+        densitylevel,
+        duedate,
+        geo,
+        kioskstate,
+        maxbikeoverflow,
+        nbbike,
+        nbbikeoverflow,
+        nbdock,
+        nbebike,
+        nbebikeoverflow,
+        nbedock,
+        nbfreedock,
+        nbfreeedock,
+        overflowactivation,
+        overflow,
+        station,
+        station_code,
+        station_name,
+        station_state,
+        station_type
+    } = data
+    console.log(geo);
+    const markerImage = require('../images/bike.png');
     return (<>
-        <Text style={{ textAlign: 'center', fontStyle: 'italic', fontSize: 30 }}>
+        <Text style={styles.title}>
             Station Details
         </Text>
         <MapView
             provider={PROVIDER_GOOGLE}
             style={styles.map}
-            /*    region={{
-                   latitude: 37.78825,
-                   longitude: -122.4324,
-                   latitudeDelta: 0.015,
-                   longitudeDelta: 0.0121,
-               }} */
             zoomEnabled
-            showsUserLocation>
+            showsUserLocation
+            initialRegion={{
+                latitude: geo[0],
+                longitude: geo[1],
+                latitudeDelta: 0.04,
+                longitudeDelta: 0.05,
+            }}
+        >
+
+            <Marker
+                coordinate={{
+                    latitude: geo[0],
+                    longitude: geo[1]
+                }}
+                /* Add a custom marker image */
+                description={"ddd"}>
+            </Marker>
 
         </MapView>
         <View style={styles.container}>
             {/* Use react vectors to add icones styles */}
             <View style={styles.listItem}>
-                <Text style={styles.textItem}>Station name: {data.station_name}</Text>
+                <Text style={styles.textItem}>Station: {station_name}</Text>
             </View>
             <View style={styles.listItem}>
-                <Text style={styles.textItem}>Station state: {data.station_state}</Text>
+                <Text style={styles.textItem}>Etat: {station_state}</Text>
             </View>
             <View style={styles.listItem}>
-                <Text style={styles.textItem}>Credit card: {data.creditcard}</Text>
+                <Text style={styles.textItem}>Paiement par carte: {creditcard}</Text>
             </View>
             <View style={styles.listItem}>
-                <Text style={styles.textItem}>Available Bikes: {data.nbbike}</Text>
+                <Text style={styles.textItem}>Vélos disponibles: {nbbike}</Text>
             </View>
         </View>
     </>)
